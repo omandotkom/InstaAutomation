52482a23dbbc2e12dc818c4b7accd13c5b9352d4
Khalid Abdurrahman <omandotkom@gmail.com>
2019-03-06 16:53:05 +0700

    Simplify initApp() method

--- src/main/java/com/dotlab/software/instaautomation/UI/MainFX.java
+++ src/main/java/com/dotlab/software/instaautomation/UI/MainFX.java
@@ -5,6 +5,7 @@
  */
 package com.dotlab.software.instaautomation.UI;
 
+import com.dotlab.software.instaautomation.Reporter.CSVReport;
 import com.dotlab.software.instaautomation.Settings.ApplicationSettings;
 import com.dotlab.software.instaautomation.UI.homepage.IntervalGenerator;
 import java.io.File;
@@ -24,15 +25,17 @@ import javafx.scene.Scene;
  */
 public class MainFX extends Application {
 
-    
     @Override
     public void start(Stage primaryStage) throws IOException, URISyntaxException {
         System.out.println("Starting... ");
-        AutoStatic.SETTINGS.loadConfigFile();
-//ApplicationSettings settings = new ApplicationSettings();
+        // AutoStatic.SETTINGS.loadConfigFile();
+
+        //ApplicationSettings settings = new ApplicationSettings();
+        ;
 
         //load like interval
-        IntervalGenerator.loadIntervals();
+        initApp();
+        
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
         //Parent root = FXMLLoader.load(getClass().getResource("/fxml/LikeModuleConfiguration.fxml"));
 
@@ -52,4 +55,16 @@ public class MainFX extends Application {
         launch(args);
     }
 
+    private void initApp() throws IOException, URISyntaxException {
+        System.out.println("InstaAutomation 1.0 Beta developed by Khalid Abdurrahman");
+        System.out.println("Initialization...");
+        System.out.println("Application path is : " + CSVReport.getJarPath());
+        System.out.println("ChromeDriver path is :" + AutoStatic.SETTINGS.getChromeDriverPath());
+        System.out.println("Like interval :" + AutoStatic.SETTINGS.getLikeInterval());
+        System.out.println("Follow interval : " + AutoStatic.SETTINGS.geetFollowInterval());
+        System.out.println("Checking application configuration...");
+        System.out.println("Is configuration valid ? : " + AutoStatic.SETTINGS.isConfigValid());
+        System.out.println("Loading intervals...");
+        IntervalGenerator.loadIntervals();
+    }
 }
