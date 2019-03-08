869e8ebc431530a49b2017442d5be9018948c28d
Khalid Abdurrahman <omandotkom@gmail.com>
2019-03-08 16:18:22 +0700

    headless & error free algorithm

--- src/main/java/com/dotlab/software/instaautomation/Automator/Automation.java
+++ src/main/java/com/dotlab/software/instaautomation/Automator/Automation.java
@@ -65,7 +65,7 @@ public class Automation {
             System.setProperty("webdriver.chrome.driver", new ApplicationSettings().getChromeDriverPath());
             ChromeOptions options = new ChromeOptions();
             //headless support
-            //options.addArguments("headless");
+            options.addArguments("headless");
             options.addArguments("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
             driver = new ChromeDriver(options);
         } catch (IOException ex) {
