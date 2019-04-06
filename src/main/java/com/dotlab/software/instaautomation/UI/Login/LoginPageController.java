f49680ea00a1e23be4f2fe771be44a6ef319c9b3
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-06 12:27:24 +0700

    add fucked up state, when instagram needs to verify credential

--- src/main/java/com/dotlab/software/instaautomation/UI/Login/LoginPageController.java
+++ src/main/java/com/dotlab/software/instaautomation/UI/Login/LoginPageController.java
@@ -25,12 +25,15 @@ import com.github.daytron.simpledialogfx.dialog.Dialog;
 import com.github.daytron.simpledialogfx.dialog.DialogType;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.stage.FileChooser;
@@ -59,6 +62,9 @@ public class LoginPageController implements Initializable {
     private Button btnMasuk;
     @FXML
     private ProgressBar progressBarLogin;
+    @FXML
+    private CheckBox headlessCheck;
+    private boolean fuckedState = false;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -83,6 +89,13 @@ public class LoginPageController implements Initializable {
             btnMasuk.setDisable(true);
         }*/
         //txtConfigPath.setText(settings.getConfigPath());
+        headlessCheck.setSelected(settings.getHeadless());
+
+        if (settings.getHeadless()) {
+            headlessCheck.setText("Headless");
+        } else {
+            headlessCheck.setText("Not Headless");
+        }
         if (settings.getUser() != null) {
             usernameTextField.setText(settings.getUser().getUsername());
             passwordTextField.setText(settings.getUser().getPassword());
@@ -90,24 +103,35 @@ public class LoginPageController implements Initializable {
         btnMasuk.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
-
-                //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                if (usernameTextField.getText().isEmpty()) {
-                    //username empty
-                    Dialog dialog = new Dialog(DialogType.ERROR, "Kesalahan", "Harap isi username instagram");
-                    dialog.showAndWait();
-                } else {
-                    if (passwordTextField.getText().isEmpty()) {
-                        //password empty
-
-                        Dialog dialog = new Dialog(DialogType.ERROR, "Kesalahan", "Harap isi password instagram");
+                if (!fuckedState) {
+                    //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    if (usernameTextField.getText().isEmpty()) {
+                        //username empty
+                        Dialog dialog = new Dialog(DialogType.ERROR, "Kesalahan", "Harap isi username instagram");
                         dialog.showAndWait();
                     } else {
-                        //Login()
-                        btnMasuk.setDisable(true);
-                        Thread loginThread = new Thread(new AuthLogin(createLoginListener()));
-                        //login(createLoginListener());
-                        loginThread.start();
+                        if (passwordTextField.getText().isEmpty()) {
+                            //password empty
+
+                            Dialog dialog = new Dialog(DialogType.ERROR, "Kesalahan", "Harap isi password instagram");
+                            dialog.showAndWait();
+                        } else {
+                            //Login()
+                            btnMasuk.setDisable(true);
+                            Thread loginThread = new Thread(new AuthLogin(createLoginListener()));
+                            //login(createLoginListener());
+                            loginThread.start();
+                        }
+                    }
+                } else {
+                    //ketika di fucked state
+                    if (AutoStatic.AUTOMATION.fuckedStateCheck().equals(AutoStatic.AUTOMATION.LOGIN_STATUS_SUCCESS)) {
+                        //ketika fucked tapi dah verifikasi dan berhasil
+                        moveToDashBoard(tmpUser);
+                    }else{
+                                Dialog dialog = new Dialog(DialogType.ERROR, "Kesalahan", "Fatal error, anda belum login. Menutup aplikasi.");
+                            dialog.showAndWait();
+                     Runtime.getRuntime().exit(0);
                     }
                 }
 
@@ -195,6 +219,34 @@ public class LoginPageController implements Initializable {
          */
     }
 
+    private void moveToDashBoard(User user) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                try {
+                    usernameTextField.setDisable(false);
+                    passwordTextField.setDisable(false);
+                    btnMasuk.setDisable(false);
+                    progressBarLogin.setProgress(1.0f);
+                    AutoStatic.LOGGED_USER = user;
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/ApplicationHomePage.fxml"));
+                    btnMasuk.getScene().getWindow().hide();
+                    Stage stage = (Stage) btnMasuk.getScene().getWindow();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+
+                } catch (IOException ex) {
+                    //  Logger.getLogger(LoginPageController.class.getName()).log(Level.SEVERE, null, ex);
+                    Dialog dialog = new Dialog(DialogType.EXCEPTION, "Kesalahan", "An error occured during login");
+                    dialog.showAndWait();
+
+                }
+            }
+        });
+
+    }
+
     private LoginListener createLoginListener() {
         LoginListener mLoginListener = new LoginListener() {
             @Override
@@ -224,8 +276,8 @@ public class LoginPageController implements Initializable {
                         passwordTextField.setDisable(false);
                         btnMasuk.setDisable(false);
                         progressBarLogin.setProgress(0.0f);
-                         Dialog dialog = new Dialog(DialogType.EXCEPTION, "Kesalahan", "Please check your internet connection, username or password.");
-                dialog.showAndWait();
+                        Dialog dialog = new Dialog(DialogType.EXCEPTION, "Kesalahan", "Please check your internet connection, username or password.");
+                        dialog.showAndWait();
                     }
                 });
 
@@ -233,29 +285,23 @@ public class LoginPageController implements Initializable {
 
             @Override
             public void onLoginSuccess(User user) {
+                 moveToDashBoard(user);
+            }
 
+            @Override
+            public void onLoginFuckedUp(User user) {
+                //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
-                        //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        try {
-                            usernameTextField.setDisable(false);
-                            passwordTextField.setDisable(false);
-                            btnMasuk.setDisable(false);
-                            progressBarLogin.setProgress(1.0f);
-                            AutoStatic.LOGGED_USER = user;
-                            Parent root = FXMLLoader.load(getClass().getResource("/fxml/ApplicationHomePage.fxml"));
-                            btnMasuk.getScene().getWindow().hide();
-                            Stage stage = (Stage) btnMasuk.getScene().getWindow();
-                            stage.setScene(new Scene(root));
-                            stage.show();
-
-                        } catch (IOException ex) {
-                            //  Logger.getLogger(LoginPageController.class.getName()).log(Level.SEVERE, null, ex);
-                            Dialog dialog = new Dialog(DialogType.EXCEPTION, "Kesalahan", "An error occured during login");
-                            dialog.showAndWait();
-
-                        }
+                        //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                        Dialog dialog = new Dialog(DialogType.ERROR, "Kesalahan", "Instagram meminta verifiksi identitas anda\n"
+                                + "Sebelum melanjutkan, lakukan langkah berikut : \n1. Pilih verifikasi kode lewat email.\n2. Buka email Anda via browser.\n3. Copy kode yang diberikan Instagram dan Paste kan pada halaman Instagram");
+                        dialog.showAndWait();
+                        tmpUser = user;
+                        fuckedState = true;
+                        btnMasuk.setText("Periksa");
+                        btnMasuk.setDisable(false);
                     }
                 });
 
@@ -263,6 +309,28 @@ public class LoginPageController implements Initializable {
 
         };
         return mLoginListener;
+    }
+private User tmpUser;
+    @FXML
+    private void headlessCheckonAction(ActionEvent event) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Konfirmasi Headless");
+        alert.setHeaderText("Headless Options");
+        alert.setContentText("Jika opsi diatur headless, maka browser tidak akan ditampilkan.\n Jika tidak diatur headless, maka browser akan ditampilkan.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            // ... user chose OK
+            new ApplicationSettings().saveHeadless(headlessCheck.isSelected());
+            Alert alert2 = new Alert(AlertType.INFORMATION);
+            alert2.setTitle("Berhasil");
+            alert2.setHeaderText("Berhasil Mengatur Headless");
+            alert2.setContentText("InstaAutomation akan tertutup, silahkan buka kembali.");
+            alert2.showAndWait();
+            Runtime.getRuntime().exit(0);
+        } else {
+            // ... user chose CANCEL or closed the dialog
+        }
+        alert.showAndWait();
     }
 
     private interface LoginListener {
@@ -272,6 +340,8 @@ public class LoginPageController implements Initializable {
         void onLoginFailed();
 
         void onLoginSuccess(User user);
+
+        void onLoginFuckedUp(User user);
     }
 
     class AuthLogin implements Runnable {
@@ -292,20 +362,24 @@ public class LoginPageController implements Initializable {
                 new ApplicationSettings().saveUser(user);
                 //Automation AUTOMATION = new AUTOMATION;
                 list.onLoginStarted();
-                if (AutoStatic.AUTOMATION.auth(new ApplicationSettings().getUser().getUsername(), new ApplicationSettings().getUser().getPassword())) {
+                /*if (AutoStatic.AUTOMATION.auth(new ApplicationSettings().getUser().getUsername(), new ApplicationSettings().getUser().getPassword())) {
                     list.onLoginSuccess(user);
+                }*/
+                if (AutoStatic.AUTOMATION.auth(new ApplicationSettings().getUser().getUsername(), new ApplicationSettings().getUser().getPassword()).equals(AutoStatic.AUTOMATION.LOGIN_STATUS_SUCCESS)) {
+                    list.onLoginSuccess(user);
+                } else {
+                    System.out.println("You need to verify your credential.");
+                    list.onLoginFuckedUp(user);
                 }
-            } catch (NoSuchElementException nse){
-            list.onLoginFailed();
-            System.out.println(nse.getMessage());
-            }
-            catch (WebDriverException wbe){
-            list.onLoginFailed();
-            System.out.println(wbe.getMessage());
-            }
-            catch (Exception ex) {
+            } catch (NoSuchElementException nse) {
                 list.onLoginFailed();
-               System.out.println(ex.getMessage());
+                System.out.println(nse.getMessage());
+            } catch (WebDriverException wbe) {
+                list.onLoginFailed();
+                System.out.println(wbe.getMessage());
+            } catch (Exception ex) {
+                list.onLoginFailed();
+                System.out.println(ex.getMessage());
 
             }
         }
